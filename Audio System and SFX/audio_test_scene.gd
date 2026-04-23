9a058854a5e6d02bd9faf75515ed30885fe49b62
Pleb-Pleb <n2EImK@gmail.com>
2026-04-23 19:51:32 +0700

    Push Finished Music State with 1 test scene

--- Audio System and SFX/audio_test_scene.gd
+++ Audio System and SFX/audio_test_scene.gd
@@ -3,8 +3,8 @@ extends Node
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		match event.keycode:
-			KEY_F6: AudioManager.set_music_state.emit("main_menu")
-			KEY_F7: AudioManager.set_music_state.emit("non_combat")
-			KEY_F8: AudioManager.set_music_state.emit("combat")
-			KEY_F9: AudioManager.set_music_state.emit("shopping")
-			KEY_F10: AudioManager.set_music_state.emit("dying")
+			KEY_LEFT:  AudioManager.set_music_state.emit("main_menu")
+			KEY_UP:    AudioManager.set_music_state.emit("non_combat")
+			KEY_RIGHT: AudioManager.set_music_state.emit("combat")
+			KEY_DOWN:  AudioManager.set_music_state.emit("shopping")
+			KEY_SPACE: AudioManager.set_music_state.emit("dying")
